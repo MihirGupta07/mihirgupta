8b081b4c987bfe0c03b7cc39b426c95ff82a9ccd
Mihir Gupta <mihirgupta0712@gmail.com>
2022-04-04 19:17:05 +0530

    header commit 2

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a
+        className="btn"
+        href="https://docs.google.com/document/d/1POI7Ub2ZdZ6yzAb02xKSUAsHQ1Ma2ybF/edit?usp=sharing&ouid=110939023280218741630&rtpof=true&sd=true"
+        target="_blank"
+      >
+        Download Resume
+      </a>
+      <a className="btn btn-primary" href="#Contact">
+        Contact me?
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Header.css";
+import me from "../../assets/IMG_51612-removebg-preview.png";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
 const about = () => {
-  return <header>
-    <div
-
-  </header>;
+  return (
+    <header>
+      <div className="container container__header">
+        <h5>Hello I m </h5>
+        <h1 className>Mihir Gupta</h1>
+        <h5 className="text-light">CSE Student at VIT Vellore</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={me} />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default about;
